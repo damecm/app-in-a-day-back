0f5b915477752229be9d3f2e9616c274d84c26f8
Nemu02 <neumv2@icloud.com>
2022-11-10 17:24:50 -0600

    pip

--- app.py
+++ app.py
@@ -152,6 +152,37 @@ if __name__ == '__main__':
 
 
 
+{
+    "name": "sit ups",
+    "category": "core",
+    "demo_img": "https://i.imgur.com/4778qbz.png"
+},
+{
+    "name": "plank",
+    "category": "core",
+    "demo_img": "https://i.imgur.com/k1bvaTr.png"
+},
+{
+    "name": "side to side",
+    "category": "core",
+    "demo_img": "https://i.imgur.com/LTO4vop.png"
+},
+{
+    "name": "flutter kicks",
+    "category": "core",
+    "demo_img": "https://i.imgur.com/DBxG2O3.png"
+},
+{
+    "name": " swimmers",
+    "category": "core",
+    "demo_img": "https://i.imgur.com/LMFTGTn.png"
+},
+{
+    "name": "bicycle kicks",
+    "category": "core",
+    "demo_img": "image.png"
+},
+
 
 
 
